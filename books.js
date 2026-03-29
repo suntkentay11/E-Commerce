d331dd0badf3ade8fb4df0d18974eb3300b89d01
suntkentay@gmail.com <suntkentay@gmail.com>
2026-03-29 12:22:02 -0500

    final JS

--- books.js
+++ books.js
@@ -1,12 +1,17 @@
-function renderBooks(filter){
+let books;
+
+async function renderBooks(filter){
     const booksWrapper = document.querySelector('.books');
 
-    const books = getBooks();
+    if (!books){
+        books = await getBooks();
+    }
+    
 
     if(filter === 'LOW_TO_HIGH'){
-        books.sort((a, b) => a.salePrice - b.salePrice)
+        books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
     } else if(filter === 'HIGH_TO_LOW'){
-        books.sort((a, b) => b.salePrice - a.salePrice)
+        books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice))
     } else if(filter === 'RATING'){
         books.sort((a, b) => b.rating - a.rating)
     }
@@ -22,13 +27,19 @@ function renderBooks(filter){
         <div class="book__ratings">
             ${ratingsHTML(book.rating)}
         </div>
-        </div>
         <div class="book__price">
-            <span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span>$${book.salePrice.toFixed(2)}
+            ${priceHTML(book.originalPrice, book.salePrice)}
         </div>
     </div>`
     }).join('')
     booksWrapper.innerHTML =  booksHtml
+}
+
+function priceHTML (originalPrice, salePrice){
+    if (!salePrice || originalPrice === salePrice){
+        return `$${originalPrice.toFixed(2)}`
+    }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`
 }
 
 function filterBooks(event){
@@ -51,126 +62,132 @@ setTimeout(() => {
 })
 
 function getBooks(){
-    return [
-        {
-            id: 1,
-            title: 'Theo of Golden: A Novel',
-            url: './assets/Theo-of-Golden.jpeg',
-            originalPrice: 20.00,
-            salePrice: 14.99,
-            rating: 4.5
-        },
-        {
-            id: 2,
-            title: 'The Vanishing Cherry Blossom Bookshop',
-            url: './assets/The-Vanishing-Cherry-Blossom-Bookshop.jpeg',
-            originalPrice: 12.99,
-            salePrice: 9.99,
-            rating: 4
-        },
-        {
-            id: 3,
-            title: 'Project Hail Mary: A Novel',
-            url: './assets/Project-Hail-Mary.jpeg',
-            originalPrice: 22.00,
-            salePrice: 18.99,
-            rating: 4.5
-        },
-        {
-            id: 4,
-            title: 'Heated Rivalry',
-            url: './assets/Heated-Rivalry.jpeg',
-            originalPrice: 18.99,
-            salePrice: 16.99,
-            rating: 4.5
-        },
-        {
-            id: 5,
-            title: 'The Correspondent: A Novel',
-            url: './assets/The-Correspondent.jpeg',
-            originalPrice: 28.00,
-            salePrice: 25.00,
-            rating: 4.5
-        },
-        {
-            id: 6,
-            title: 'Wuthering Heights',
-            url: './assets/Wuthering-Heights.jpeg',
-            originalPrice: 9.00,
-            salePrice: 6.99,
-            rating: 4
-        },
-        {
-            id: 7,
-            title: 'The Let Them Theory',
-            url: './assets/The-Let-Them-Theory.jfif',
-            originalPrice: 29.99,
-            salePrice: 24.99,
-            rating: 3
-        },
-        {
-            id: 8,
-            title: 'The Golden Boy',
-            url: './assets/The-Golden-Boy.jpeg',
-            originalPrice: 29.00,
-            salePrice: 16.00,
-            rating: 4.5
-        },
-        {
-            id: 9,
-            title: 'Vigil: A Novel',
-            url: './assets/vigil.jpeg',
-            originalPrice: 28.00,
-            salePrice: 25.00,
-            rating: 4
-        },
-        {
-            id: 10,
-            title: 'The Unlikely Tale of Chase & Finnegan',
-            url: './assets/The-Unlikely-Tale-of-Chase&Finnegan.jpeg',
-            originalPrice: 18.99,
-            salePrice: 15.99,
-            rating: 4.5
-        },
-        {
-            id: 11,
-            title: 'The Best Dog in the World',
-            url: './assets/The-Best-Dog-in-the-World.jpeg',
-            originalPrice: 22.00,
-            salePrice: 18.00,
-            rating: 5
-        },
-        {
-            id: 12,
-            title: 'Once and Again: A Novel',
-            url: './assets/Once-and-Again.jpeg',
-            originalPrice: 27.00,
-            salePrice: 24.00,
-            rating: 3.5
-        },
-        {
-            id: 13,
-            title: 'My Husband’s Wife: A Novel',
-            url: './assets/My-Husband\'s-Wife.jpeg',
-            originalPrice: 28.99,
-            salePrice: 25.99,
-            rating: 4
-        },
-        {
-            id: 14,
-            title: 'Operation Bounce House',
-            url: './assets/Operation-Bounce-House.jpeg',
-            originalPrice: 32.00,
-            salePrice: 29.00,
-            rating: 4.5
-        },
-        {
-            id: 15,
-            title: 'The Crossroads',
-            url: './assets/The-Crossroads.jpeg',
-            originalPrice: 32.00,
-            salePrice: 22.40,
-            rating: 5
-        },
-    ]
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(
+                [
+                    {
+                        id: 1,
+                        title: 'Theo of Golden: A Novel',
+                        url: './assets/Theo-of-Golden.jpeg',
+                        originalPrice: 20.00,
+                        salePrice: 14.99,
+                        rating: 4.5
+                    },
+                    {
+                        id: 2,
+                        title: 'The Vanishing Cherry Blossom Bookshop',
+                        url: './assets/The-Vanishing-Cherry-Blossom-Bookshop.jpeg',
+                        originalPrice: 12.99,
+                        salePrice: 9.99,
+                        rating: 4
+                    },
+                    {
+                        id: 3,
+                        title: 'Project Hail Mary: A Novel',
+                        url: './assets/Project-Hail-Mary.jpeg',
+                        originalPrice: 22.00,
+                        salePrice: null,
+                        rating: 4.5
+                    },
+                    {
+                        id: 4,
+                        title: 'Heated Rivalry',
+                        url: './assets/Heated-Rivalry.jpeg',
+                        originalPrice: 18.99,
+                        salePrice: 16.99,
+                        rating: 4.5
+                    },
+                    {
+                        id: 5,
+                        title: 'The Correspondent: A Novel',
+                        url: './assets/The-Correspondent.jpeg',
+                        originalPrice: 28.00,
+                        salePrice: 25.00,
+                        rating: 4.5
+                    },
+                    {
+                        id: 6,
+                        title: 'Wuthering Heights',
+                        url: './assets/Wuthering-Heights.jpeg',
+                        originalPrice: 9.00,
+                        salePrice: 6.99,
+                        rating: 4
+                    },
+                    {
+                        id: 7,
+                        title: 'The Let Them Theory',
+                        url: './assets/The-Let-Them-Theory.jfif',
+                        originalPrice: 29.99,
+                        salePrice: 24.99,
+                        rating: 3
+                    },
+                    {
+                        id: 8,
+                        title: 'The Golden Boy',
+                        url: './assets/The-Golden-Boy.jpeg',
+                        originalPrice: 29.00,
+                        salePrice: 16.00,
+                        rating: 4.5
+                    },
+                    {
+                        id: 9,
+                        title: 'Vigil: A Novel',
+                        url: './assets/vigil.jpeg',
+                        originalPrice: 28.00,
+                        salePrice: null,
+                        rating: 4
+                    },
+                    {
+                        id: 10,
+                        title: 'The Unlikely Tale of Chase & Finnegan',
+                        url: './assets/The-Unlikely-Tale-of-Chase&Finnegan.jpeg',
+                        originalPrice: 18.99,
+                        salePrice: 15.99,
+                        rating: 4.5
+                    },
+                    {
+                        id: 11,
+                        title: 'The Best Dog in the World',
+                        url: './assets/The-Best-Dog-in-the-World.jpeg',
+                        originalPrice: 22.00,
+                        salePrice: 18.00,
+                        rating: 5
+                    },
+                    {
+                        id: 12,
+                        title: 'Once and Again: A Novel',
+                        url: './assets/Once-and-Again.jpeg',
+                        originalPrice: 27.00,
+                        salePrice: 24.00,
+                        rating: 3.5
+                    },
+                    {
+                        id: 13,
+                        title: 'My Husband’s Wife: A Novel',
+                        url: './assets/My-Husband\'s-Wife.jpeg',
+                        originalPrice: 28.99,
+                        salePrice: null,
+                        rating: 4
+                    },
+                    {
+                        id: 14,
+                        title: 'Operation Bounce House',
+                        url: './assets/Operation-Bounce-House.jpeg',
+                        originalPrice: 32.00,
+                        salePrice: null,
+                        rating: 4.5
+                    },
+                    {
+                        id: 15,
+                        title: 'The Crossroads',
+                        url: './assets/The-Crossroads.jpeg',
+                        originalPrice: 32.00,
+                        salePrice: 22.40,
+                        rating: 5
+                    },
+                ]
+            )
+        }, 1000)
+    })
 }
